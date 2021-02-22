a0db7e288f1da9782883e97fa2bb93798493ae79
imbrikis <imbrikis@yahoo.com>
2021-02-22 13:08:25 -0700

    styling: app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Container } from 'react-bootstrap';
 
 const App = () => {
   return (
-    <div className="App">
+    <Container fluid='md'>
       App
-    </div>
+    </Container>
   );
 }
 
